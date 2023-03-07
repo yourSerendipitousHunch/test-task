500f4b1458645fcef6290f33b7f7c66443c3c692
Anton <krava34007@gmail.com>
2023-03-07 17:10:52 +0200

    add ability to leave a comment

--- assets/script/custom.js
+++ assets/script/custom.js
@@ -1,14 +1,75 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Get the "Ok" button in #p_modal3
+document.addEventListener("DOMContentLoaded", function () {
   var okButton = document.querySelector("#p_modal_button3");
 
-  // Attach an event listener to the "Ok" button
-  okButton.addEventListener("click", function() {
-    // Retrieve the user's responses from the input fields
+  okButton.addEventListener("click", function () {
     var response = document.querySelector("#userResponse").value;
-
-    // Output the user's responses to the browser console
     console.log(response);
   });
-});
 
+  // The ability to leave a comment
+
+  const commentSection = document.querySelector('.comments_face');
+  const commentInput = document.getElementById('comment-input');
+  const submitButton = document.getElementById('submit-comment');
+
+  submitButton.addEventListener('click', function () {
+    const comment = commentInput.value;
+  
+    const newComment = document.createElement('div');
+    newComment.classList.add('comments');
+    newComment.setAttribute('id', `comment${commentSection.children.length}`);
+    newComment.style.display = 'block';
+  
+    // Set the innerHTML of the new comment element
+    newComment.innerHTML = `
+    <div class="profile">
+    <img src="assets/img/anonymous.webp"></div>
+  <div class="comment-content">
+    <p class="name">
+      <font style="vertical-align: inherit;">
+        <font style="vertical-align: inherit;">Inês</font>
+      </font>
+    </p>
+    <p>
+      <font style="vertical-align: inherit;">
+        <font style="vertical-align: inherit;">Eu ganhei nada</font>
+      </font>
+    </p>
+  </div>
+  <div class="clr"></div>
+  <div class="comment-status">
+    <span>
+      <font style="vertical-align: inherit;">
+        <font style="vertical-align: inherit;">Curte·comente</font>
+      </font>
+      <img src="assets/img/3.webp" width="15px" height="15px">
+      <font style="vertical-align: inherit;">
+        <font style="vertical-align: inherit;">29</font>
+      </font>
+    </span>
+    <font style="vertical-align: inherit;">
+      <small>
+        <font style="vertical-align: inherit;">·</font>
+      </small>
+
+
+      <small>
+        <u>
+          <font style="vertical-align: inherit;">4 minutos antes</font>
+        </u>
+      </small>
+    </font>
+    <small>
+      <font style="vertical-align: inherit;"></font>
+      <u>
+        <font style="vertical-align: inherit;"></font>
+      </u>
+    </small>
+  </div>
+      `
+
+    commentSection.insertBefore(newComment, commentSection.children[1]);
+    commentInput.value = '';
+  });
+
+});
